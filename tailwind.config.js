97ba3f01adb6b2ccbd621a519cf45fe4e61ae380
ClementJosse <clement.b.josse@gmail.com>
2025-06-23 15:51:38 +0200

    purge du helloworld, installation de tailwind et import des couleurs

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'lightblue': '#E6F6FF',
+        'blue': '#68AEFF',
+        'strongblue': '#193AB7',
+        'lightred': '#FFEBEB',
+        'red': '#FF686C',
+        'strongred': '#830C41',
+        'base1': '#FFFAF3',
+        'base2': '#FFF1DE',
+        'base3': "#FFD5A4",
+        'primary': "#AE3D00",
+        'secondary': "#FFA63F"
+      }
+    },
+  },
+  plugins: [],
+}
+
