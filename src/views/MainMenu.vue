efa88435ba2c15e89a2925315ddef74971bddb81
trafalgxrlaw <trafalgar.minato@gmail.com>
2025-06-24 18:05:56 +0200

    Join game from main menu

--- src/views/MainMenu.vue
+++ src/views/MainMenu.vue
@@ -1,29 +1,30 @@
 <template>
     <div class="flex flex-col items-center ">
-        <button class="mt-[400px] px-10 py-3 bg-secondary rounded-lg text-base1 inter-bold text-xl"
+        <button class="mt-[360px] px-10 py-3 bg-secondary rounded-lg text-base1 inter-bold text-xl"
             @click='createGame()'>
             Créer une partie
         </button>
-
+        <span v-if="existe" class="text-secondary text-xl">Parties disponibles</span>
+        <button v-for="(partie, code) in gameData" :key="code" class="bg-base2 rounded-2xl px-6 py-2 text-center mt-7 w-[290px] h-[70px]" @click="joinGame(code)">              <!-- itération dans la clé (de la game), chaque partie (que je définis en l.15) -->
+            <p class="text-2xl text-primary inter-bold">{{ Object.values(partie.idToPlayer).length }} Joueurs</p>                                                               <!-- nombre de value dans idToPlayer (length) = nb joueurs -->
+            <p class="text-secondary font-medium">Créé par :<span class="text-primary inter-bold">{{ (partie.idToPlayer)[partie.leader]}}</span></p>                            <!-- affiche le id du UID = leader -->
+        </button>
     </div>
 </template>
 
-
 <script setup>
 import router from '../router';
-import { useRoute } from 'vue-router'
 import { ref, onMounted } from 'vue'
-import { getDatabase, ref as dbRef, onValue, set, get, child, serverTimestamp } from 'firebase/database'
+import { getDatabase, ref as dbRef, onValue, set, get, child} from 'firebase/database'
 import { getAuth, signInAnonymously } from 'firebase/auth'
 
 const auth = getAuth()
 let UID = null
 
-const route = useRoute()
-const gameId = route.params.gameId
 const database = getDatabase()
-const partieRef = dbRef(database, `/${gameId}`)
+const rootRef = dbRef(database, `/`)
 const gameData = ref(null)
+const existe = ref(false)
 
 // Fonction d'initialisation asynchrone
 const initialize = async () => {
@@ -33,9 +34,10 @@ const initialize = async () => {
         UID = auth.currentUser.uid
         console.log("Authentification anonyme :", UID)
         // Écoute des changements sur la partie dans Firebase
-        onValue(partieRef, (snapshot) => {
+        onValue(rootRef, (snapshot) => {
             const data = snapshot.val()
             gameData.value = data
+            existe.value = data && Object.keys(data).length > 0
         })
 
     } catch (error) {
@@ -75,4 +77,8 @@ const createGame = () => {
         console.error('Erreur lors de la création de la partie:', error);
     }
 }
+const joinGame = (code) => {
+  router.push("/${code}")
+}
+
 </script>
