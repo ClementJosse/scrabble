24aad625f2e17e37ba40752250eae4c76ade77e0
trafalgxrlaw <trafalgar.minato@gmail.com>
2025-06-26 01:45:57 +0200

    timer statique

--- src/components/TimerTurn.vue
+++ src/components/TimerTurn.vue
@@ -1,5 +1,17 @@
 <template>
-    <div>
-        timer
+    <div class="flex bg-base2 rounded-2xl px-8 py-6 justify-between items-center w-[330px] absolute top-6 right-6 shadow-md shadow-base3">
+        <div class="flex flex-col">
+            <span class="text-secondary text-xl font-semibold">Tour </span>
+            <span class="text-primary text-5xl font-bold mt-1">0:01</span>
+        </div>
+        <div class="flex flex-col w-auto">
+            <span class="text-primary font-semibold">Au tour de :</span>
+            <div class="bg-primary text-base1 font-semibold px-4 py-1 rounded-full mt-1 w-fit">Clément</div>
+            <span class="text-primary text-base mt-3">Suivant : <span class="font-semibold">Lucas</span>
+            </span>
+        </div>
     </div>
 </template>
+
+<script setup>
+</script>
