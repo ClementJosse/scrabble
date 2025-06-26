77453b1b7220d3188008d60716b427ba663a0d70
ClementJosse <clement.b.josse@gmail.com>
2025-06-27 00:48:55 +0200

    base de la structure du dico et import des dicos du dicoscrabble.cjosse.com

--- src/components/Dico.vue
+++ src/components/Dico.vue
@@ -1,19 +1,39 @@
 <template>
     <div class="fixed top-[125px] left-6 ">
         <div v-if="isLeader" class="mb-10 ml-4">
-            <img   src="@/assets/lobby.svg" alt="return to lobby" class="cursor-pointer rounded-lg"
+            <img src="@/assets/lobby.svg" alt="return to lobby" class="cursor-pointer rounded-lg"
                 @click="showReturnMenu()" />
         </div>
         <img src="@/assets/dico.svg" alt="return to lobby" class="cursor-pointer rounded-lg ml-4 relative z-20"
             @click="showDico()" />
-        <div v-if="isDicoVisible" class="bg-base2 w-[330px] h-screen relative z-10 -top-[60px] h-[calc(100vh-216px)] shadow-md shadow-base3 rounded-xl">
-            <!-- contenu -->
+        <div v-if="isDicoVisible"
+            class="bg-base2 w-[330px] h-screen relative z-10 -top-[60px] h-[calc(100vh-216px)] shadow-md shadow-base3 rounded-xl">
+            <div class="pt-20 text-sm text-secondary flex flex-col items-center">
+                *basé sur l’ODS 9 du 1er janvier 2024
+                <input ref="input" v-model="mot" type="text" placeholder="Rechercher un mot..."
+                    class="w-[300px] rounded-lg shadow-md shadow-base3 p-3 text-primary inter-bold text-xl text-bold bg-base1 border-none outline-none placeholder-base3 placeholder-opacity-100 placeholder:font-medium" />
+            </div>
+            <div>
+                <span><!-- Est valide au Scrabble/ n'est pas valide au Scrabble --></span>
+                <div>
+                    <!-- Définition du mot -->
+                </div>
+                <span>Propositions de mots</span>
+                <div>
+                    <!-- mettre ici tous les mots commençant par "mot.value" -->
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+//import { dicodef } from '../dico/dicodef';
+//import { ods9 } from '../dico/ods9.js';
+
+const mot = ref("")
+
 
 const props = defineProps({
     isLeader: {
@@ -35,6 +55,7 @@ function showReturnMenu() {
 
 <style>
 .custom-height {
-  height: calc(100vh - 110px); /* ajuste ici selon ton besoin */
+    height: calc(100vh - 110px);
+    /* ajuste ici selon ton besoin */
 }
 </style>
