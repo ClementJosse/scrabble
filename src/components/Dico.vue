d68ad95ed8b3962f8a0d1ddd3857f72cf578e3c2
trafalgxrlaw <trafalgar.minato@gmail.com>
2025-06-27 11:33:12 +0200

    transition corrigé

--- src/components/Dico.vue
+++ src/components/Dico.vue
@@ -5,7 +5,7 @@
                 @click="showReturnMenu()" />
         </div>
         <img src="@/assets/dico.svg" alt="return to lobby" class="cursor-pointer rounded-lg ml-4 relative z-20"
-            @click="showDico()" />
+            @click="showDico()" />   <Transition :duration="550" name="toggletransition">
         <div v-if="isDicoVisible"
             class="bg-base2 w-[330px] h-screen relative z-10 -top-[60px] h-[calc(100vh-216px)] shadow-md shadow-base3 rounded-xl">
             <div class="pt-20 text-sm text-secondary flex flex-col items-center">
@@ -54,5 +54,20 @@ function showReturnMenu() {
 .custom-height {
     height: calc(100vh - 110px);
     /* ajuste ici selon ton besoin */
+}
+</style>
+
+<style>
+.toggletransition-enter-active, .toggletransition-leave-active {
+	transition: all 0.3s ease-in-out;
+}
+.toggletransition-leave-active {
+  transition-delay: 0.25s;
+}
+
+.toggletransition-enter-from,
+.toggletransition-leave-to {
+  transform: translateY(30px);
+  opacity: 0;
 }
 </style>
