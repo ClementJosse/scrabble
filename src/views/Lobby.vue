554f6b0a8396554ecf6e44ee9b51e517f7e5259c
ClementJosse <clement.b.josse@gmail.com>
2025-07-04 12:16:59 +0200

    suppression temporaire de la lettre blanche

--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -51,7 +51,7 @@ const board = {}
 const playerLetters = {}
 const playerOrder = {}
 const scores = {}
-const bagTile = { _: 2, A: 9, B: 2, C: 2, D: 3, E: 15, F: 2, G: 2, H: 2, I: 8, J: 1, K: 1, L: 5, M: 3, N: 6, O: 6, P: 2, Q: 1, R: 6, S: 6, T: 6, U: 6, V: 2, W: 1, X: 1, Y: 1, Z: 1 };
+const bagTile = { A: 9, B: 2, C: 2, D: 3, E: 15, F: 2, G: 2, H: 2, I: 8, J: 1, K: 1, L: 5, M: 3, N: 6, O: 6, P: 2, Q: 1, R: 6, S: 6, T: 6, U: 6, V: 2, W: 1, X: 1, Y: 1, Z: 1 };
 const partieRef = dbRef(database, `/${gameId}`)
 
 function setupGame() {
