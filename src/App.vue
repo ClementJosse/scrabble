50444d6422805703e98f225c3a2c74f2a50bb82e
Clément Josse <86595295+ClementJosse@users.noreply.github.com>
2025-08-12 11:34:28 +0200

    modification de la taille du logo (#1)

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <div>
       <router-link to="/" class="fixed top-4 left-4 z-50">
         <div class="flex items-center space-x-2 ">
-          <img src="@/assets/logo.svg" alt="Logo de scrabble" />
-          <span class="text-secondary font-bold text-3xl">Scrabble</span>
+          <img src="@/assets/logo.svg" class="w-16" alt="Logo de scrabble" />
+          <span class="text-secondary font-bold text-2xl">Scrabble</span>
         </div>
       </router-link>
     </div>
